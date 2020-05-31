7e8900d8b50f172daa1e6c93aa5ebc504c974611
petrenkonikita112263 <bl'qyfz_rfGCekrf_t@ame>
2020-05-31 12:30:51 +0300

    Improved login page and action, added logout button, new depencies allow run application not even on Tomcat but on Weblogic

--- src/main/java/ua/spring/app/config/security/AppSecurityConfig.java
+++ src/main/java/ua/spring/app/config/security/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package ua.spring.app.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -20,4 +21,17 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("nataly").password("woman90_60_90").roles("DIRECTOR"));
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                .loginPage("/showLoginPage")
+                .loginProcessingUrl("/authenticateTheCustomer")
+                .permitAll()
+            .and()
+                .logout()
+                .permitAll();
+    }
 }
--- src/main/webapp/WEB-INF/view/home.jsp
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -1,9 +1,16 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
 <head>
     <title>Home Page</title>
 </head>
     <body>
-        Welcome to the home page of this web app.
+        <p>
+            Welcome to the home page of this web app.
+        </p>
+        <br>
+        <form:form action="${pageContext.request.contextPath}/logout" method="post">
+            <input type="submit" value="Logout" />
+        </form:form>
     </body>
 </html>
