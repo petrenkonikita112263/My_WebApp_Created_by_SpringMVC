a5f262c44ae09211f394f5e668538178d65c815e
petrenkonikita112263 <bl'qyfz_rfGCekrf_t@ame>
2020-06-16 18:14:10 +0300

    Fixed issue #5 [APP] - created additional method in service class that parse String value to Double, the main functionality of this method was covered into try/catch block, catch NumberFormatException if it's null, the value will be Types.DOUBLE in SQL alternatively it will parse into Double

--- src/main/java/ua/spring/app/service/OrderService.java
+++ src/main/java/ua/spring/app/service/OrderService.java
@@ -1,13 +1,17 @@
 package ua.spring.app.service;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.spring.app.dao.OrderDao;
 
 import java.sql.Date;
+import java.sql.Types;
 
 @Service
 public class OrderService implements Orderable {
+
+    private static final Logger LOGGER = Logger.getLogger(OrderService.class);
 
     @Autowired
     private OrderDao orderDao;
@@ -26,12 +30,21 @@ public class OrderService implements Orderable {
     @Override
     public void editFlightTicket(Integer id, String number, int flightId, String text, String startDate,
                                  String endDate, String price) {
-        orderDao.editFlightTicket(id, number, flightId, text, startDate, endDate, Double.parseDouble(price));
+        orderDao.editFlightTicket(id, number, flightId, text, startDate, endDate, parseValue(price));
     }
 
     @Override
     public void buyTicket(String customerName, Date orderTime, String price, String discount, String finalPrice, int ticketId) {
-        orderDao.addOrderTicket(customerName, orderTime, Double.parseDouble(price),
-                Double.parseDouble(discount), Double.parseDouble(finalPrice), ticketId);
+        orderDao.addOrderTicket(customerName, orderTime, parseValue(price),
+                parseValue(discount), parseValue(finalPrice), ticketId);
+    }
+
+    private double parseValue(String numberValue) {
+        try {
+            return Double.parseDouble(numberValue);
+        } catch (NumberFormatException e) {
+            LOGGER.error("The value that was parsed is null", e);
+            return Types.DOUBLE;
+        }
     }
 }
